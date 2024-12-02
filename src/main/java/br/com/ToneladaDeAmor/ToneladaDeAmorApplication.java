6163f0ae3ead32c8185b81d976841231d82886cf
Luiz Felipe <fffelippe51413121lippe@gmail.com>
2024-12-02 14:49:17 -0300

    init spring-boot

---
+++ src/main/java/br/com/ToneladaDeAmor/ToneladaDeAmorApplication.java
@@ -0,0 +1,13 @@
+package br.com.ToneladaDeAmor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToneladaDeAmorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToneladaDeAmorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ToneladaDeAmor/ToneladaDeAmorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ToneladaDeAmor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToneladaDeAmorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
