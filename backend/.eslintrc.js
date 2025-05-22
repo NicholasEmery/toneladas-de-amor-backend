ff8b782152ad4a4e74f06e80f5cd60d40be0907b
Nicholas Emery <108698936+NicholasEmery@users.noreply.github.com>
2025-05-22 12:57:03 +0000

    feat: adicionar interceptor para remover senhas de respostas e ajustar a configuração do ESLint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,26 +1,47 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: 'tsconfig.json',
+    project: "tsconfig.json",
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ["@typescript-eslint/eslint-plugin"],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended", // Garante integração com Prettier
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: [".eslintrc.js"],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'linebreak-style': ['off'],
+    // Boas práticas TypeScript
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn", // Evite 'any', mas não bloqueia
+    "@typescript-eslint/explicit-function-return-type": "off", // Deixe o TS inferir
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+
+    // Padronização de código
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false,
+        trailingComma: "all",
+        endOfLine: "auto",
+        semi: true,
+        tabWidth: 2,
+        useTabs: false,
+        bracketSpacing: true,
+        arrowParens: "always",
+        quoteProps: "consistent",
+      },
+    ],
+
+    // Outras regras úteis
+    "no-console": "warn",
+    "linebreak-style": "off", // Evita conflito entre SOs
   },
 };
