459f3341f7da31f96a6a496acae9387490a874c5
Nicholas Emery <nicholasemery10@gmail.com>
2025-06-28 09:09:07 -0300

    refactor: atualizar o DTO UpdateDonationDto para corrigir a validação de status e adicionar descrições nas propriedades

--- backend/src/donation/dto/updateDonation/updateDonation.dto.ts
+++ backend/src/donation/dto/updateDonation/updateDonation.dto.ts
@@ -1,20 +1,33 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Status } from "@prisma/client";
 import { IsMongoId, IsNotEmpty, IsString, Matches } from "class-validator";
 
 export class UpdateDonationDto {
+  @ApiProperty({
+    default: "Donation ID",
+    description: "ID of the donation to be updated",
+  })
   @IsNotEmpty()
   @IsString()
   @IsMongoId()
   donationId!: string;
 
+  @ApiProperty({
+    default: "User ID",
+    description: "ID of the user making the donation",
+  })
   @IsNotEmpty()
   @IsString()
   @IsMongoId()
   userId!: string;
 
+  @ApiProperty({
+    default: "COMFIRMED",
+    description: "Status of the donation",
+  })
   @IsNotEmpty()
-  @Matches(/^COMPLETED|FAILED$/, {
-    message: "Status must be 'COMPLETED' or 'FAILED'",
+  @Matches(/^COMFIRMED|CANCELED$/, {
+    message: "Status must be either 'COMFIRMED' or 'CANCELED'",
   })
   status!: Status;
 }
